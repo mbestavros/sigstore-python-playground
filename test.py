4bc6ba222f5548be3c6a532bd93fc6e10eb8a89d
Mark Bestavros <mbestavr@redhat.com>
2022-08-23 13:54:19 -0400

    Create tool to import and sign keylime allowlist artifacts

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import base64
-import hashlib
+import getopt
 import merkle
 import sigstore
+import sys
 import json
 
 from cryptography.hazmat.primitives import hashes
@@ -13,7 +14,8 @@ from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from os.path import exists
 
-def main():
+def main(argv):
+
     # --- CREATE KEYS ---
 
     if not exists("private.key"):
@@ -35,71 +37,78 @@ def main():
     with open("public.pem", "wb") as pem_out:
         pem_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo))
 
+    # get paths
+    allowlist = None
+    try:
+        opts, args = getopt.getopt(argv,"ha:",["allowlistfile="])
+    except getopt.GetoptError:
+        print('importer.py -a <allowlist>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("test.py -a <allowlistfile>")
+            sys.exit()
+        elif opt in ("-a", "--allowlistfile"):
+            allowlist = arg
+
     print(" --- SIGNING STEP --- ")
 
-    artifact = "Sigstore is the future!!"
+    with open(allowlist, "r") as f:
+        alist_raw = f.read()
 
-    upload_response = sigstore.sign_offline_and_upload(private_key, artifact)
+    tlog_policy = {}
+    for line in alist_raw.splitlines()[:10]:
+        line = line.strip()
+        if len(line) == 0:
+            continue
 
-    print("Rekor upload response:")
-    print(json.dumps(json.loads(upload_response["response"].content), indent=4, sort_keys=True))
+        pieces = line.split(None, 1)
+        if not len(pieces) == 2:
+            print("Line in Allowlist does not consist of hash and file path: %s", line)
+            continue
+
+        (checksum_hash, path) = pieces
+
+        tlog_policy[path] = checksum_hash
+
+        print(f"Uploading path: {path}")
+
+        with open(path, "rb") as artifact:
+            upload_response = sigstore.sign_offline_and_upload(private_key, artifact)
 
     print()
     print(" --- VERIFICATION STEP - HASH SEARCH --- ")
 
-    artifact_hash = hashlib.sha256(artifact.encode()).hexdigest()
+    for artifact_hash in tlog_policy:
 
-    search_response = sigstore.search(hash=artifact_hash)
+        search_response = sigstore.search(hash=artifact_hash)
 
-    uuids = json.loads(search_response.content)
+        uuids = json.loads(search_response.content)
 
-    print('Found UUIDs matching provided artifact hash. Verifying...')
+        print('Found UUIDs matching provided artifact hash. Verifying...')
 
-    for uuid in uuids:
+        for uuid in uuids:
 
-        fetch_uuid_response = sigstore.fetch_with_uuid(uuid=uuid)
+            fetch_uuid_response = sigstore.fetch_with_uuid(uuid=uuid)
 
-        entries = json.loads(fetch_uuid_response.content)
-        for key in entries.keys():
-            entry = entries[key]
-        encoded_rekord = entry["body"]
-        rekor_cert = json.loads(base64.b64decode(encoded_rekord))['spec']['signature']['content']
+            entries = json.loads(fetch_uuid_response.content)
+            for key in entries.keys():
+                entry = entries[key]
+            encoded_rekord = entry["body"]
+            rekor_cert = json.loads(base64.b64decode(encoded_rekord))['spec']['signature']['content']
 
-        try:
-            public_key.verify(base64.b64decode(rekor_cert), artifact.encode(), ec.ECDSA(hashes.SHA256()))
-            print(f'{uuid}: Signature validation: PASS')
-        except:
-            print(f'{uuid}: Signature validation: FAIL')
+            try:
+                public_key.verify(base64.b64decode(rekor_cert), artifact.encode(), ec.ECDSA(hashes.SHA256()))
+                print(f'{uuid}: Signature validation: PASS')
+            except:
+                print(f'{uuid}: Signature validation: FAIL')
 
-        try:
-            merkle.verify_merkle_inclusion(entry)
-            print("Inclusion proof verified!")
-        except merkle.InvalidInclusionProofError as e:
-            print(e)
+            try:
+                merkle.verify_merkle_inclusion(entry)
+                print("Inclusion proof verified!")
+            except merkle.InvalidInclusionProofError as e:
+                print(e)
 
-    print()
-    print(" --- VERIFICATION STEP - INPUTS SEARCH --- ")
-
-    artifact_signature = upload_response["signature"]
-    fetch_inputs_response = sigstore.fetch_with_inputs(artifact_signature, public_key, artifact_hash)
-    entries = json.loads(fetch_inputs_response.content)[0]
-    print("Retrieved entries:")
-    print(json.dumps(entries, indent=4, sort_keys=True))
-    for entry in entries.keys():
-        entry_encoded = entries[entry]['body']
-        rekor_cert = json.loads(base64.b64decode(entry_encoded))['spec']['signature']['content']
-
-        try:
-            public_key.verify(base64.b64decode(rekor_cert), artifact.encode(), ec.ECDSA(hashes.SHA256()))
-            print('Artifact signature verification against Rekor passed!')
-        except:
-            print('Artifact signature verification against Rekor failed!')
-
-        try:
-            merkle.verify_merkle_inclusion(entries[entry])
-            print("Inclusion proof verified!")
-        except merkle.InvalidInclusionProofError as e:
-            print(e)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
